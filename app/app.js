8be1657d5803f0a8813ec23f94cbed8791243c2b
Anyanwu Chimebuka Emmanuel <80542129+chimebukanian@users.noreply.github.com>
2024-02-23 22:32:31 +0100

    Update app.js cors

--- app/app.js
+++ app/app.js
@@ -31,7 +31,7 @@ app.use("/", authRouter);
 app.use(express.static(path.join(__dirname, "build")));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
     optionSuccessStatus:200
